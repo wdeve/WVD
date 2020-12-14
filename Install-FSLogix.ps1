7fea9448710a883866280e6a11a02105ae520d4b
Wim Vermeer <wim.vermeer@vtmgroep.nl>
2020-12-14 16:22:15 +0100

    Added comments and remove unesesary line

--- Install-FSLogix.ps1
+++ Install-FSLogix.ps1
@@ -1,6 +1,8 @@
 $url = "https://aka.ms/fslogix_download"
 $output = "FSlogix.zip"
+#Dowload FSlogiix
 Start-BitsTransfer -Source $url -Destination $PSScriptroot\$output
-(New-Object System.Net.WebClient).DownloadFile($url, $output)
+#Unzip the zip file
 Expand-Archive -LiteralPath $output -DestinationPath "$PSScriptroot\FSLogix" -erroraction Silentlycontinue
+#Install FSLogix
 & "$PSScriptroot/FSLogix/x64/Release/FSLogixAppsSetup.exe" /install /quiet /norestart
