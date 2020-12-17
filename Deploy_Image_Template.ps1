16c84e563c54e68f4cf9fef3a3a1a52d4a43ee3b
Wim Vermeer <wim.vermeer@vtmgroep.nl>
2020-12-17 11:47:29 +0100

    added code for registration and to purge the envoirment created

--- Deploy_Image_Template.ps1
+++ Deploy_Image_Template.ps1
@@ -11,7 +11,7 @@ $imageTemplateName = "Test_WVD_Image"
 
 Add-Content -Path $roledefFile -Value @"
 {
-    "Name": "Azure WVD Image builder",
+    "Name": "Azure Image builder",
     "IsCustom": true,
     "Description": "Image Builder access to create resources for the image build, you should delete or split out as appropriate",
     "Actions": [
@@ -30,34 +30,45 @@ Add-Content -Path $roledefFile -Value @"
     ]
 }
 "@
+New-AzResourceGroup -Name "$imageResourceGroup" -Location $Location 
+
 New-AzUserAssignedIdentity -Name "WVD_ImageBuilder" -ResourceGroupName $idenityRG
 
 New-AzRoleDefinition -InputFile $roledefFile
 Remove-Item -Path $roledefFile
-
+Start-Sleep -Seconds 20
 ##$idenityNameResourceId = $(Get-AzUserAssignedIdentity -ResourceGroupName $idenityRG -Name $idenityName).Id
 ##$ClientId = $(Get-AzUserAssignedIdentity -ResourceGroupName $idenityRG -Name $idenityName).ClientId
 $idenityNamePrincipalId = $(Get-AzUserAssignedIdentity -ResourceGroupName $idenityRG -Name $idenityName).PrincipalId
 
-New-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName "Azure WVD Image builder" -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
+New-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName "Azure Image builder" -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
 
-New-AzResourceGroup -Name "$imageResourceGroup" -Location $Location 
 New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $ImageTemplateFile -imageTemplateName $imageTemplateName 
-
-$RGs = Get-AzResourceGroup
-$RGs | Where-Object {$_.ResourceGroupName -eq "IT_Images_Test_WVD_Image_928e78af-307c-47de-9aee-f8907a0298c3"} | Remove-AzResourceGroup -Force
-$RGs | Where-Object {$_.ResourceGroupName -eq "Images"} | Remove-AzResourceGroup -Force
-
 Invoke-AzResourceAction -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2020-02-14" -Action Run -Force
 
 #Get Status of the Image Build and Query
 ##$resourcetowatch = Get-AzResource -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -Name $imageTemplateName
+Write-Host -NoNewline "Waiting  "
 do {
     $status = (Get-AzResource -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -Name $imageTemplateName).Properties.lastRunStatus
-    $status | Format-Table *
+    Write-Host -NoNewline "."
     Start-Sleep -Seconds 30
 } while ($status.runState -eq "Running")
+Write-Host -ForegroundColor Green "`nFinally done"
+(Get-AzResource -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -Name $imageTemplateName).Properties.lastRunStatus
 
+
+break 
+
+
+
+Test-AzResourceGroupDeployment -ResourceGroupName Temp -TemplateFile $ImageTemplateFile -imageTemplateName $imageTemplateName 
+
+
+
+
+
+###Extra conde
 Register-AzProviderFeature -ProviderNamespace Microsoft.VirtualMachineImages -FeatureName VirtualMachineTemplatePreview
 do {
     $status = (Get-AzProviderFeature -ProviderNamespace Microsoft.VirtualMachineImages -FeatureName VirtualMachineTemplatePreview).RegistrationState
@@ -65,4 +76,25 @@ do {
     Start-Sleep -Seconds 30
 } while ($status -eq "Registering")
 
-(Get-AzResource –ResourceGroupName RG_WVD_AzureImageBuilder -ResourceType Microsoft.VirtualMachineImages/imageTemplates -Name $ImageTemplateName).Properties.lastRunStatus
+(Get-AzResource -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -Name $imageTemplateName).Properties.lastRunStatus
+Get-AzImageBuilderTemplate -ImageTemplateName  $imageTemplateName -ResourceGroupName $imageResourceGroup | Select-Object -ExpandProperty LastRunStatus| Select-Object -ExpandProperty Message
+
+
+Get-AzResourceProvider -ProviderNamespace Microsoft.Compute, Microsoft.KeyVault, Microsoft.Storage, Microsoft.VirtualMachineImages |
+  Where-Object RegistrationState -ne Registered |
+    Register-AzResourceProvider
+
+New-AzImageBuilderTemplate -Source
+
+
+
+
+
+#Envoirment cleanup
+$RGs = Get-AzResourceGroup
+$RGs | Where-Object {$_.ResourceGroupName -like "IT_Images_Test_WVD_Image"} | Remove-AzResourceGroup -Force
+$RGs | Where-Object {$_.ResourceGroupName -eq "Images"} | Remove-AzResourceGroup -Force
+##Remove-AzResourceGroup ImageBuilderRG -Force
+
+Remove-AzUserAssignedIdentity -Name "WVD_ImageBuilder" -ResourceGroupName $idenityRG -Force
+Remove-AzRoleDefinition -Name "Azure Image builder" -Force
