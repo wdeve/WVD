08ffd0f64c0663d829bf5835f2ad505ae85599ee
Wim Vermeer <wim.vermeer@vtmgroep.nl>
2020-12-21 14:44:17 +0100

    Started using managed image and changed cmdlet for deploying

--- Image/Deploy-Image_TotalDeployment.ps1
+++ Image/Deploy-Image_TotalDeployment.ps1
@@ -1,7 +1,9 @@
 $ImageTemplateFile = ".\Image\JSON_Builds\Total_Image_Deployment.JSON"
 $location = "West Europe"
 
-Test-AzDeployment -Location $location -TemplateFile $ImageTemplateFile
+Test-AzSubscriptionDeployment  -Location $location -TemplateFile $ImageTemplateFile
 
 
 New-AzDeployment -Location $location -Name "Total_Image_Deployment" -TemplateFile $ImageTemplateFile
+
+New-AzSubscriptionDeployment  -Location $location -Name "Total_Image_Deployment" -TemplateFile $ImageTemplateFile
